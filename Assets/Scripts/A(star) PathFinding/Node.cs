80d88e7c2ac41fdff861b9b11d5855567da62131
Dothi <jyrihonkakoski@gmail.com>
2016-03-31 09:54:34 +0300

    jee

--- Assets/Scripts/A(star) PathFinding/Node.cs
+++ Assets/Scripts/A(star) PathFinding/Node.cs
@@ -6,6 +6,9 @@ public class Node
     public bool walkable;
     public Vector3 worldPosition;
 
+    public int gCost;
+    public int hCost;
+
     public Node(bool _walkable, Vector3 _worldPosition)
     {
         walkable = _walkable;
---
+++ Assets/Scripts/A(star) PathFinding/Pathfinding.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Pathfinding : MonoBehaviour
+{
+    Grid grid;
+
+    void Awake()
+    {
+        grid = GetComponent<Grid>();
+    }
+    void FindPath(Vector3 startPos, Vector3 targetPos)
+    {
+        Node startNode = grid.NodeFromWorldPoint(startPos);
+        Node targetNode = grid.NodeFromWorldPoint(targetPos);
+
+        List<Node> openSet = new List<Node>();
+        HashSet<Node> closedSet = new HashSet<Node>();
+        openSet.Add(startNode);
+
+        while (openSet.Count > 0)
+        {
+            Node currentNode = openSet[0];
+
+        }
+    }
+}
