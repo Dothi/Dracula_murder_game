3e95daadb905b356aca84af5adde2f4ca140de21
Vegetabbel <veikkahuttunen@kamk.fi>
2016-04-22 09:23:27 +0300

    Minigame aluilleen
    Uus skene minigamen testaamiselle
    Minigame resettyy aina ko Canvaksessa olevan "Kill Minigame" objektin
    disablee ja enablee

---
+++ Assets/Scripts/KillGameButton.cs
@@ -0,0 +1,93 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+
+[System.Serializable]
+public class LetterSprites
+{
+    public Sprite A;
+    public Sprite S;
+    public Sprite D;
+    public Sprite Z;
+    public Sprite X;
+    public Sprite C;
+}
+
+public class KillGameButton : MonoBehaviour {
+
+    public LetterSprites sprites = new LetterSprites();
+    float fadeRange = 250;
+
+    Image image;
+    KeyCode keyCode = KeyCode.A;
+    string letter = "A";
+
+	void Start ()
+    {
+        image = GetComponent<Image>();
+	}	
+
+	void Update ()
+    {
+        ColorFade();
+	}
+
+    public void RandomizeLetter()          //Maybe implement it so that it doesn't random same letter many times in a row
+    {
+        int r = Random.Range(0, 6);
+        switch (r)
+        {
+            case 0:
+                image.sprite = sprites.A;
+                keyCode = KeyCode.A;
+                letter = "A";
+                break;
+            case 1:
+                image.sprite = sprites.S;
+                keyCode = KeyCode.S;
+                letter = "S";
+                break;
+            case 2:
+                image.sprite = sprites.D;
+                keyCode = KeyCode.D;
+                letter = "D";
+                break;
+            case 3:
+                image.sprite = sprites.Z;
+                keyCode = KeyCode.Z;
+                letter = "Z";
+                break;
+            case 4:
+                image.sprite = sprites.X;
+                keyCode = KeyCode.X;
+                letter = "X";
+                break;
+            case 5:
+                image.sprite = sprites.C;
+                keyCode = KeyCode.C;
+                letter = "C";
+                break;
+            default:
+                break;
+        }
+    }
+    
+    public KeyCode getKeyCode()
+    {
+        return keyCode;
+    }
+
+    void ColorFade()
+    {
+        if (transform.position.x == transform.parent.position.x)
+        {
+            image.color = new Color(image.color.r, image.color.g, image.color.b, 1f);
+        }
+        else if (transform.position.x < transform.position.x + fadeRange && transform.position.x > transform.parent.position.x)
+        {
+            image.color = new Color(image.color.r, image.color.g, image.color.b, 1 - (transform.localPosition.x / fadeRange));
+        }
+    }
+}
---
+++ Assets/Scripts/KillMinigame.cs
@@ -0,0 +1,125 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using System.Collections.Generic;
+
+public class KillMinigame : MonoBehaviour {
+
+    List<KeyCode> keyCodes = new List<KeyCode>();
+    KeyCode CancelKey = KeyCode.Space;
+
+    int MinigameLength = 10;
+    int currentButton = 0;
+    float buttonGap = 2;
+
+    public GameObject killGameButtonPrefab;
+    List<KillGameButton> buttonList = new List<KillGameButton>();
+
+    GameObject buttonParent;
+
+    void Start()
+    {
+        SetKeyCodesList();
+        buttonParent = transform.Find("Buttons").gameObject;
+
+        SpawnButtons();
+        SetButtonPositions();       
+    }
+
+	void OnEnable ()
+    {
+        //TO DO:
+        //Set the minigame above player position here
+        //Disable player movement and other input stuff
+        SetButtonPositions();
+        currentButton = 0;
+
+        for (int i = 0; i < buttonList.Count; i++)
+        {
+            buttonList[i].GetComponent<Image>().enabled = true;
+            buttonList[i].RandomizeLetter();
+        }
+	}
+
+    void Update()
+    {
+        if (Input.anyKeyDown)
+        {
+            if (Input.GetKeyDown(CancelKey))
+            {
+                //TO DO: Cancel minigame
+            }
+            else
+            {
+                for (int i = 0; i < keyCodes.Count; i++)
+                {
+                    if (Input.GetKeyDown(buttonList[currentButton].getKeyCode()))
+                    {
+                        //Correct button!
+                        buttonList[currentButton].GetComponent<Image>().enabled = false;
+                        currentButton++;
+                        if (currentButton == MinigameLength)
+                        {
+                            //TO DO: winning
+                            Debug.Log("Minigame won!");
+                            break;
+                        }
+                        //Move buttons - Maybe add smooth movement (lerp)?
+                        for (int j = 0; j < buttonList.Count; j++)
+                        {
+                            KillGameButton button = buttonList[j];
+                            if (button.GetComponent<Image>().enabled == true)
+                            {
+                                button.transform.position = new Vector2(button.transform.position.x - (button.GetComponent<RectTransform>().rect.width + buttonGap),
+                                        buttonParent.transform.position.y);
+                            }
+                        }
+                        break;
+                    }
+                    if (Input.GetKeyDown(keyCodes[i]))
+                    {
+                        //TO DO: Fail
+                        Debug.Log("Minigame failed!");
+                        break;
+                    }
+                }
+            }
+        }
+	}
+    void SpawnButtons()
+    {
+        for (int i = 0; i < MinigameLength; i++)
+        {
+            GameObject button = (GameObject)Instantiate(killGameButtonPrefab, buttonParent.transform.position, Quaternion.identity);
+            button.transform.SetParent(buttonParent.transform);
+            buttonList.Add(button.GetComponent<KillGameButton>());
+        }
+    }
+    void SetButtonPositions()
+    {
+        for (int i = 0; i < buttonList.Count; i++)
+        {
+            GameObject button = buttonList[i].gameObject;
+            if (i == 0)
+            {
+                //Set position of first button
+                button.transform.position = buttonParent.transform.position;
+            }
+            else
+            {                                          //Position of last button               + width of button                                 + assigned gap
+                button.transform.position = new Vector2(buttonList[i - 1].transform.position.x + button.GetComponent<RectTransform>().rect.width + buttonGap,
+                                                        buttonParent.transform.position.y);
+            }
+        }
+    }
+
+    void SetKeyCodesList()
+    {
+        keyCodes.Add(KeyCode.A);
+        keyCodes.Add(KeyCode.S);
+        keyCodes.Add(KeyCode.D);
+        keyCodes.Add(KeyCode.Z);
+        keyCodes.Add(KeyCode.X);
+        keyCodes.Add(KeyCode.C);
+    }
+}
