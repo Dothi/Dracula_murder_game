c43b31e63dc6d14364532ae65a1afb8e759c92f6
Vegetabbel <veikkahuttunen@kamk.fi>
2016-05-17 13:17:03 +0300

    Menujuttuu

---
+++ Assets/Scripts/MenuController.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MenuController : MonoBehaviour {
+
+    public void LoadMenu()
+    {
+        Application.LoadLevel(0);
+    }
+    public void LoadLevel()
+    {
+        Application.LoadLevel(1);
+    }
+    public void LoadCredits()
+    {
+        Application.LoadLevel(2);
+    }
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+}
